9235d223cbda99e9d818dc08be6a9e3809e03247
satonogohannnn <tosshy.0513@gmail.com>
2024-08-22 01:05:18 +0000

    add the controllers

---
+++ book-api/src/main/java/com/example/book_api/Controllers/HealthController.java
@@ -0,0 +1,18 @@
+package com.example.book_api.Controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(HealthController.URL)
+public class HealthController {
+    
+    public static final String URL = "/health";
+
+    @GetMapping("")
+    public ResponseEntity<String> getHealth() {
+        return ResponseEntity.ok("Book API is up and running!");
+    }
+}
